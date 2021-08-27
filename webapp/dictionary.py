610a990bae1d9fd29c58ef99431f8959f249964d
Yohannes Getu <nede.evoli@gmail.com>
2021-08-27 11:35:36 +0300

    implemented getting output instantly

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -14,18 +14,16 @@ class Dictionary:
         jp.Div(a=div, text="Get the definition of any English word instaltly as you type.", classes="text-lg")
 
         input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
         input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
                              classes="m-2 bg-gray-100 border-2 boarder-gray-200 rounded w-64 focus:outline-none "
-                                     "focus:border-purple-500 focus:bg-white py-2 px-4")
-
-        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
-        jp.Button(a=input_div, text="Ger Definition", classes="boarder-2 text-gray-500", click=cls.get_definition,
-                  outputdiv=output_div, inputbox=input_box)
+                                     "focus:border-purple-500 focus:bg-white py-2 px-4",
+                             outputdiv=output_div)
+        input_box.on('input', cls.get_definition)
 
         return wp
 
     @staticmethod
     def get_definition(widget, msg):
-        term = widget.inputbox.value
-        defined = definition.Definition(term).get()
+        defined = definition.Definition(widget.value).get()
         widget.outputdiv.text = " ".join(defined)
