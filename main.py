bacff09e940d817418ec33908650aca818e0cfc9
Yohannes Getu <nede.evoli@gmail.com>
2021-08-27 10:53:55 +0300

    Created Dictionary pages

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import justpy as jp
 
 from webapp.about import About
+from webapp.dictionary import Dictionary
 from webapp.home import Home
 
 jp.Route(path=Home.path, func_to_run=Home.serve)
 jp.Route(path=About.path, func_to_run=About.serve)
+jp.Route(path=Dictionary.path, func_to_run=Dictionary.serve)
 
 jp.justpy(port=8001)
