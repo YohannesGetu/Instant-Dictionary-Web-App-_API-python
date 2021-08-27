117dd943a7847311e473692ef77fa62d00d2ef84
Yohannes Getu <nede.evoli@gmail.com>
2021-08-27 11:30:21 +0300

    getting the definition in the output box

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,5 +1,7 @@
 import justpy as jp
 
+import definition
+
 
 class Dictionary:
     path = "/dictionary"
@@ -11,9 +13,19 @@ class Dictionary:
         jp.Div(a=div, text="Instant English Dictionary", classes="text-4xl m-2")
         jp.Div(a=div, text="Get the definition of any English word instaltly as you type.", classes="text-lg")
 
-        jp.Input(a=div, placeholder="Type in a word here...",
-                 classes="m-2 bg-gray-100 border-2 boarder-gray-200 rounded w-64 focus:outline-none "
-                         "focus:border-purple-500 focus:bg-white py-2 px-4")
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
+                             classes="m-2 bg-gray-100 border-2 boarder-gray-200 rounded w-64 focus:outline-none "
+                                     "focus:border-purple-500 focus:bg-white py-2 px-4")
+
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        jp.Button(a=input_div, text="Ger Definition", classes="boarder-2 text-gray-500", click=cls.get_definition,
+                  outputdiv=output_div, inputbox=input_box)
 
         return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        term = widget.inputbox.value
+        defined = definition.Definition(term).get()
+        widget.outputdiv.text = " ".join(defined)
